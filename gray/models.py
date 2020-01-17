e383f53292668f209f0ba1de0bfbd1e9cb75fbe6
chunweitian <chunweitian@163.com>
2020-01-17 15:57:47 +0800

    initial commit

---
+++ gray/models.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+
+class Conv_BN_Relu_first(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,padding,groups,bias):
+        super(Conv_BN_Relu_first,self).__init__()
+        kernel_size = 3
+        padding = 1
+        features = 64
+        groups =1 
+        self.conv = nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size, padding=padding,groups=groups, bias=False)
+        self.bn = nn.BatchNorm2d(features)
+        self.relu = nn.ReLU(inplace=True)
+    def forward(self,x):
+        return self.relu(self.bn(self.conv(x)))
+
+class Conv_BN_Relu_other(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,padding,groups,bias):
+        super(Conv_BN_Relu_other,self).__init__()
+        kernel_size = 3
+        padding = 1
+        features = out_channels
+        groups =1 
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=features, kernel_size=kernel_size, padding=padding,groups=groups, bias=False)
+        self.bn = nn.BatchNorm2d(features)
+        self.relu = nn.ReLU(inplace=True)
+    def forward(self,x):
+        return self.relu(self.bn(self.conv(x)))
+
+
+class Conv(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,padding,groups,bais):
+        super(Conv,self).__init__()
+        kernel_size = 3
+        padding = 1
+        features = 1
+        groups =1 
+        self.conv = nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size, padding=padding,groups=groups, bias=False)
+    def forward(self,x):
+        return self.conv(x)
+
+class Self_Attn(nn.Module):
+    def __init__(self,in_dim):
+        super(Self_Attn,self).__init__()
+        self.chanel_in = in_dim
+        self.query_conv = nn.Conv2d(in_channels=in_dim,out_channels=in_dim//8,kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim,out_channels=in_dim//8,kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim,out_channels=in_dim,kernel_size=1)
+        self.gamma=nn.Parameter(torch.zeros(1))
+        self.softmax=nn.Softmax(dim=-1)
+    def forward(self,x):
+        m_batchsize, C, width,height = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1)
+        proj_key = self.key_conv(x).view(m_batchsize,-1,width*height)
+        print proj_query.size()
+        print proj_key.size()
+        print '5'
+        energy = torch.bmm(proj_query,proj_key)
+        print '6'
+        #print energy.size()
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) 
+        print '1'
+        out = torch.bmm(proj_value,attention.permute(0,2,1))
+        print '2'
+        out = out.view(m_batchsize,C,width,height)
+        out = self.gamma*out + x
+        return out, attention
+
+class ADNet(nn.Module):
+    def __init__(self, channels, num_of_layers=15):
+        super(ADNet, self).__init__()
+        kernel_size = 3
+        padding = 1
+        features = 64
+        groups =1 
+        layers = []
+        kernel_size1 = 1
+        '''
+        #self.gamma = nn.Parameter(torch.zeros(1))
+        '''
+        self.conv1_1 = nn.Sequential(nn.Conv2d(in_channels=channels,out_channels=features,kernel_size=kernel_size,padding=padding,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_2 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=2,groups=groups,bias=False,dilation=2),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_3 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_4 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_5 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=2,groups=groups,bias=False,dilation=2),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_6 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_7 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=padding,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_8 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_9 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=2,groups=groups,bias=False,dilation=2),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_10 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_11 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_12 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=2,groups=groups,bias=False,dilation=2),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_13 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=padding,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_14 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=padding,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_15 = nn.Sequential(nn.Conv2d(in_channels=features,out_channels=features,kernel_size=kernel_size,padding=1,groups=groups,bias=False),nn.BatchNorm2d(features),nn.ReLU(inplace=True))
+        self.conv1_16 = nn.Conv2d(in_channels=features,out_channels=1,kernel_size=kernel_size,padding=1,groups=groups,bias=False)
+        self.conv3 = nn.Conv2d(in_channels=2,out_channels=1,kernel_size=1,stride=1,padding=0,groups=1,bias=True)
+        self.ReLU = nn.ReLU(inplace=True)
+        self.Tanh= nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, (2 / (9.0 * 64)) ** 0.5)
+            if isinstance(m, nn.BatchNorm2d):
+                m.weight.data.normal_(0, (2 / (9.0 * 64)) ** 0.5)
+                clip_b = 0.025
+                w = m.weight.data.shape[0]
+                for j in range(w):
+                    if m.weight.data[j] >= 0 and m.weight.data[j] < clip_b:
+                        m.weight.data[j] = clip_b
+                    elif m.weight.data[j] > -clip_b and m.weight.data[j] < 0:
+                        m.weight.data[j] = -clip_b
+                m.running_var.fill_(0.01)
+    def _make_layers(self, block,features, kernel_size, num_of_layers, padding=1, groups=1, bias=False):
+	layers = []
+        for _ in range(num_of_layers):
+            layers.append(block(in_channels=features, out_channels=features, kernel_size=kernel_size, padding=padding, groups=groups, bias=bias))
+        return nn.Sequential(*layers)
+    def forward(self, x):
+        input = x 
+        x1 = self.conv1_1(x)
+        x1 = self.conv1_2(x1)
+        x1 = self.conv1_3(x1)
+        x1 = self.conv1_4(x1)
+        x1 = self.conv1_5(x1)
+        x1 = self.conv1_6(x1)
+        x1 = self.conv1_7(x1)   
+        x1t = self.conv1_8(x1)
+        x1 = self.conv1_9(x1t)
+        x1 = self.conv1_10(x1)
+        x1 = self.conv1_11(x1)
+        x1 = self.conv1_12(x1)
+        x1 = self.conv1_13(x1)
+        x1 = self.conv1_14(x1)
+        x1 = self.conv1_15(x1)
+        x1 = self.conv1_16(x1)
+        out = torch.cat([x,x1],1)
+        out= self.Tanh(out)
+        out = self.conv3(out)
+        out = out*x1
+        out2 = x - out
+        return out2
